137b98e1ed455af47794835113e263cd0e8788c4
ubuntu <amir.ubaidillah@gmail.com>
2024-09-27 17:39:11 +0700

    Change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import PrelineScript from "./components/PrelineScript";
 import { BusRouteProvider } from "./lib/Context";
 import { Footer } from "./components/Footer";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,6 +20,15 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Calo Digital | Cari Jadwal Bus",
   description: "Bus Schedule Listing | Project By Amir Ubaidillah",
+  icons: {
+    icon: "/bus-ico.png",
+    shortcut: "/bus-ico.png",
+    apple: "/bus-ico.png",
+    other: {
+      rel: "apple-touch-icon",
+      url: "/bus-ico.png",
+    },
+  },
 };
 
 export default function RootLayout({
@@ -27,16 +37,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-300 min-h-screen`}
-      >
-        <div className="sm:container sm:mx-auto min-h-screen p-8 sm:max-w-screen-sm md:max-w-screen-lg lg:max-w-screen-lg bg-white relative">
-          <BusRouteProvider>{children}</BusRouteProvider>
-        </div>
-        <Footer />
-      </body>
-      <PrelineScript />
-    </html>
+    <>
+      <Head>
+        <link rel="icon" href="/bus-ico.png" type="image/png" sizes="32x32" />
+      </Head>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-300 min-h-screen`}
+        >
+          <div className="sm:container sm:mx-auto min-h-screen p-8 sm:max-w-screen-sm md:max-w-screen-lg lg:max-w-screen-lg bg-white relative">
+            <BusRouteProvider>{children}</BusRouteProvider>
+          </div>
+          <Footer />
+        </body>
+        <PrelineScript />
+      </html>
+    </>
   );
 }
