7d9e7dbb461f916b46e9617d8c344926d9da92e7
ubuntu <amir.ubaidillah@gmail.com>
2024-09-27 17:53:01 +0700

    Delete backend dir

--- backend/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import busRoute from './busRoute'
-import busName from './busName'
-import busClass from './busClass'
-import city from './city'
-import busStop from './busStop'
-
-export const schemaTypes = [busRoute, busName, busClass, city, busStop]
