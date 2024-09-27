a4a3ecbb9dc2b453e62bb171712c4ba20d0613ef
ubuntu <amir.ubaidillah@gmail.com>
2024-09-27 15:33:06 +0700

    add footer

--- app/components/Tab.tsx
+++ app/components/Tab.tsx
@@ -8,7 +8,7 @@ export const Tab = ({
   station: React.ReactNode;
 }) => {
   return (
-    <div>
+    <div className="shadow-xl rounded-2xl">
       <nav
         className="flex gap-x-1"
         aria-label="Tabs"
@@ -17,7 +17,7 @@ export const Tab = ({
       >
         <button
           type="button"
-          className="hs-tab-active:bg-orange-600 hs-tab-active:text-white hs-tab-active:hover:text-white hs-tab-active:dark:text-white py-3 px-4 text-center basis-0 grow inline-flex justify-center items-center gap-x-2 bg-transparent text-sm font-medium text-gray-500 hover:text-orange-600 focus:outline-none focus:text-orange-600 rounded-t-lg disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400 dark:hover:text-neutral-300 dark:focus:text-neutral-300 active border border-gray-300 bg-orange-50"
+          className="hs-tab-active:bg-orange-600 hs-tab-active:text-white hs-tab-active:hover:text-white hs-tab-active:dark:text-white py-3 px-4 text-center basis-0 grow inline-flex justify-center items-center gap-x-2 bg-transparent text-sm font-medium text-gray-500 hover:text-orange-600 focus:outline-none focus:text-orange-600 rounded-t-lg disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400 dark:hover:text-neutral-300 dark:focus:text-neutral-300 active border border-orange-400 bg-orange-50"
           id="equal-width-elements-item-1"
           aria-selected="true"
           data-hs-tab="#equal-width-elements-1"
@@ -28,7 +28,7 @@ export const Tab = ({
         </button>
         <button
           type="button"
-          className="hs-tab-active:bg-orange-600 hs-tab-active:text-white hs-tab-active:hover:text-white hs-tab-active:dark:text-white py-3 px-4 text-center basis-0 grow inline-flex justify-center items-center gap-x-2 bg-transparent text-sm font-medium text-gray-500 hover:text-orange-600 focus:outline-none focus:text-orange-600 rounded-t-lg disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400 dark:hover:text-neutral-300 dark:focus:text-neutral-300 border border-gray-300 bg-orange-50"
+          className="hs-tab-active:bg-orange-600 hs-tab-active:text-white hs-tab-active:hover:text-white hs-tab-active:dark:text-white py-3 px-4 text-center basis-0 grow inline-flex justify-center items-center gap-x-2 bg-transparent text-sm font-medium text-gray-500 hover:text-orange-600 focus:outline-none focus:text-orange-600 rounded-t-lg disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400 dark:hover:text-neutral-300 dark:focus:text-neutral-300 border border-orange-400 bg-orange-50"
           id="equal-width-elements-item-3"
           aria-selected="false"
           data-hs-tab="#equal-width-elements-3"
@@ -39,7 +39,7 @@ export const Tab = ({
         </button>
       </nav>
 
-      <div className="p-4 border-2 rounded-b-2xl">
+      <div className="p-4 border-0 rounded-b-2xl">
         <div
           id="equal-width-elements-1"
           role="tabpanel"
